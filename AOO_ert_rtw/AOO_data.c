ccfca6ebd59ae0b9c9579238419f3831ba8aeb9b
OlukemiOdujinrin <odujinro@mcmaster.ca>
2022-10-03 16:30:31 -0400

    aoo works

---
+++ AOO_ert_rtw/AOO_data.c
@@ -0,0 +1,44 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: AOO_data.c
+ *
+ * Code generated for Simulink model 'AOO'.
+ *
+ * Model version                  : 1.11
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Mon Oct  3 16:29:12 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "AOO.h"
+
+/* Block parameters (default storage) */
+P_AOO_T AOO_P = {
+  /* Expression: 100
+   * Referenced by: '<Root>/p_lowrateinterval'
+   */
+  100.0,
+
+  /* Expression: 1
+   * Referenced by: '<Root>/p_aPaceWidth'
+   */
+  1.0,
+
+  /* Expression: 100
+   * Referenced by: '<Root>/p_aPaceAmp'
+   */
+  100.0
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
