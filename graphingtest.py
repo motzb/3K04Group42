25065ccbf2681318225167cce8036bd710faa719
motzb <motzb@mcmaster.ca>
2022-11-26 16:58:38 -0500

    made changes to gfunc

--- graphingtest.py
+++ graphingtest.py
@@ -2,17 +2,22 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 #function takes in the time array and signal array
-def graphdata(time, signal):
-    #creates axis titles and graph title
-    plt.title("Heart Signal") 
-    plt.xlabel("time") 
-    plt.ylabel("bpm") 
+def graphdata(time, signalatr,signalvent):
     #plots and shows the graph
-    plt.plot(time, signal, color ="red") 
+    fig, axs = plt.subplots(2) 
+    axs[0].plot(time,signalatr)
+    axs[0].set_title("Atrial Signal")
+    axs[0].set(xlabel="time")
+    axs[0].set(ylabel="heart signal")
+    axs[1].plot(time,signalvent)
+    axs[1].set_title("Ventricle Signal")
+    axs[1].set(xlabel="time")
+    axs[1].set(ylabel="heart signal")
+    plt.tight_layout()
     plt.show()
-
 
 #test case
 x=[1,2,3,4,5,6]
-y=[60,62,57,88,69,90]
-graphdata(x,y)
+y=[0,62,57,88,69,90]
+y2=[60,62,57,88,69,90]
+graphdata(x,y,y2)
